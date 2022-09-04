861fd2e2f79d774bf38f23bd8e38dffea56c5bd6
Adam-Fosdick <adamfosdickmgr@gmail.com>
2022-09-04 14:51:51 -0700

    w5_Learn_Together

---
+++ w5/app.js
@@ -0,0 +1,47 @@
+const showOnPage = function (text) {
+  let newParagraph = document.createElement("p");
+  newParagraph.innerHTML = text;
+  let outputDiv = document.getElementById("output");
+  outputDiv.append(newParagraph);
+};
+
+
+const choice = [
+  {
+    time: "4:45 am",
+    body: " I am going back to sleep its to early",
+  },
+  {
+    time: "5:15 am",
+    body: " Its a little early but we can still ride",
+  },
+  {
+    time: "5:30 am",
+    body: " I woke up right on time",
+  },
+  {
+    time: "5:45 am",
+    body: " Its a little late but we can still ride",
+  },
+  {
+    time: "6:00 am",
+    body: " Its to late to ride I need to rush to work",
+  },
+];
+
+const day = [
+  "monday",
+  "tuesday",
+  "wednesday",
+  "thursday",
+  "friday",
+  "saturday",
+  "sunday ",
+];
+
+for (count = 0; count < 6; count++) {
+  let min = 0;
+  let max = 4;
+  let x = Math.floor(Math.random() * (max - min + 1)) + min;
+  showOnPage(`Today is ${day[count]}, Its ${choice[x].time} and ${choice[x].body}`);
+}
