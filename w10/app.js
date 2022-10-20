830fb8ffc00390de841d0f5026bbec74bb6e8b7c
Adam-Fosdick <adamfosdickmgr@gmail.com>
2022-10-20 12:00:51 -0700

    reform

---
+++ w10/app.js
@@ -0,0 +1,42 @@
+const now = moment();
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let message = [];
+  let error = 0;
+  const id = uuidv4();
+  if (fast.checked === true && slow.checked === true) {
+    message.push("You can't physically take two cars");
+  }
+  if (fast.checked === false && slow.checked === false) {
+    message.push(" You must pick which car your going to drive");
+  }
+  if (left.value === " ") {
+    message.push(" please select a time to leave");
+  }
+  if (t.value === " ") {
+    message.push(" please pick a traffic pattern");
+  }
+
+  if (message.length !== 0) {
+    const errorEl = document.createElement("div");
+    errorEl.textContent = `${message.join(" , ")}`;
+    document.getElementById("error").appendChild(errorEl);
+    error = -1;
+  } else {
+    console.log(now.toString());
+    let sportscar = form.elements.fast.checked;
+    let familycar = form.elements.slow.checked;
+    let timeLeft = form.elements.left.value;
+    let traffic = form.elements.t.value;
+    let newObj = {
+      id: id,
+      sportscar: sportscar,
+      familycar: familycar,
+      timeLeft: timeLeft,
+      traffic: traffic,
+    };
+    convert(newObj);
+    makeOutcome(newObj);
+    document.getElementById("form").reset();
+  }
+});
---
+++ w10/functions.js
@@ -0,0 +1,116 @@
+const outcome = [];
+let total = 0;
+outcome.forEach(function (each) {});
+const makeOutcome = function (obj) {
+  let message = {
+    output: " ",
+    id: obj.id,
+  };
+  if (total === 0) {
+    message.output = " ";
+  } else if (total > 3) {
+    message.output = `you left at ${obj.timeLeft} and the traffic was ${
+      obj.traffic
+    } and your early for work. You entered in this information at ${now.format(
+      "MMMM Do, YYYY"
+    )}`;
+  } else if (total < 3 && total > 0) {
+    message.output = `you left at ${obj.timeLeft} and the traffic was ${
+      obj.traffic
+    } and your late for work.  You entered in this information at ${now.format(
+      "MMMM Do, YYYY"
+    )}`;
+  } else {
+    message.output = `you left at ${obj.timeLeft} and the traffic was ${
+      obj.traffic
+    } and your right on time.  You entered in this information at ${now.format(
+      "MMMM Do, YYYY"
+    )}`;
+  }
+
+  if (error === -1) {
+    message.output = " ";
+  } else {
+    outcome.push(message);
+    renderChoice(outcome);
+  }
+};
+
+let convert = function (obj) {
+  let ttotal = 0;
+  let ltotal = 0;
+  let ctotal = 0;
+  if (obj.traffic === "light") {
+    ttotal = ttotal + 2;
+  } else if (obj.traffic === "normal") {
+    ttotal = ttotal + 1;
+  }
+
+  if (obj.timeLeft === "5:00 am") {
+    ltotal = ltotal + 2;
+  } else if (obj.timeLeft === "5:30 am") {
+    ltotal = ltotal + 1;
+  }
+
+  if (obj.sportscar === true) {
+    ctotal = ctotal + 2;
+  } else if (obj.familycar === true) {
+    ctotal = ctotal + 1;
+  }
+  total = ltotal + ttotal + ctotal;
+
+  let x = {
+    newtotal: total,
+  };
+  return x;
+};
+
+const renderChoice = function (outcome) {
+  document.querySelector("#decision-output").innerHTML = "";
+  if (outcome !== 0) {
+    outcome.forEach(function (outcome) {
+      const messageEl = document.createElement("h3");
+      messageEl.textContent = `${outcome.output}`;
+      document.querySelector("#decision-output").appendChild(messageEl);
+
+      myRemove();
+    });
+  } else {
+    const noInputEl = document.createElement("h3");
+    noInputEl.textContent = "Please create an input";
+    document.querySelector("#decision-output").appendChild(noInputEl);
+  }
+};
+
+const saveMessage = function (outcome) {
+  localStorage.setItem("outcome", JSON.stringify(outcome));
+};
+
+const myRemove = function () {
+  const remove = document.createElement("button");
+  remove.textContent = "delete The message above ^";
+  document.querySelector("#decision-output").appendChild(remove);
+  const removeT = document.createElement("hr");
+  removeT.textContent = `${now.fromNow()}`;
+  document.querySelector("#decision-output").appendChild(removeT);
+  const edit = document.createElement("button");
+  edit.textContent = "Edit";
+  document.querySelector("#decision-output").appendChild(edit);
+  edit.addEventListener("click", function () {
+    location.assign(`edit.html`);
+  });
+  remove.addEventListener("click", function () {
+    saveMessage(outcome);
+    removeMessage(outcome);
+    renderChoice(outcome);
+  });
+};
+
+const removeMessage = function (id) {
+  const index = outcome.findIndex(function () {
+    return outcome === id;
+  });
+  if (index > -1) {
+    outcome.splice(index, 1);
+  }
+};
